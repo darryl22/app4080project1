b9f25c9b9b02a6b58d52efc9617f7bab7c15c700
Darryl A. Mbatia <dmbatia@usiu.ac.ke>
2022-02-10 12:04:12 +0300

    second changes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let encodedtext;
 let decodedtext;
 
-function encode() {
+function encode(text) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let temp = "";
 
+    for (let a = 0; a < text.length; a++){
+        let position
+        for(let b = 0; b < alphabet.length; b++){
+            if (text[a] == alphabet[b]){
+                temp = temp + alphabet[b + 2];
+            }
+        }
+    }
+    return temp;
 }
 
 function decode() {
-    
+
 }
+
+console.log(encode("aac"))
